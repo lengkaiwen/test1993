d8e9201eeaa23eb9ff1b801e5e0f4cd44a428606
leng <251596784@qq.com>
2020-06-30 22:12:01 +0800

    0001

---
+++ src/main/java/com/leng/campmaket/CampMaketApplication.java
@@ -0,0 +1,13 @@
+package com.leng.campmaket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampMaketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampMaketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leng/campmaket/CampMaketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leng.campmaket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampMaketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
